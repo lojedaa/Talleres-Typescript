426ffdcc116ded1de87590f592e45acfd7ba4364
lojedaa <88736587+lojedaa@users.noreply.github.com>
2022-11-07 12:23:27 -0500

    Add files via upload

---
+++ Serie.ts
@@ -0,0 +1,4 @@
+export class Serie{
+    constructor(public id: number, public name: string, public chanel: string, public seasons: number, public description: string, public link: string, public image: string){
+    }
+}
---
+++ main.ts
@@ -0,0 +1,55 @@
+import {Serie} from "./Serie.js";
+import { dataSeries } from "./data.js";
+console.log(dataSeries);
+
+let seriesTable = document.getElementById("id_series")!;
+let promedioTable = document.getElementById("promedio")!;
+let serieCard= document.getElementById("cardSerie")!;
+
+datosSeries(dataSeries);
+promedio(dataSeries);
+
+function datosSeries(array: Serie[]):void{
+    let seriesTbody: HTMLElement = document.createElement("tbody");
+    for(let sr of array){
+        let trElement: HTMLElement = document.createElement("tr");
+        trElement.innerHTML = `<th scope="row">${sr.id}</th>
+        <td><input type="button" value="${sr.name}" id=${sr.id} class="btn" style="color:blue"></input></td>
+        <td>${sr.chanel}</td>
+        <td>${sr.seasons}</td>`;
+        seriesTbody.appendChild(trElement);
+    }
+    seriesTable.appendChild(seriesTbody);
+}
+
+function promedio(array: Serie[]):void{
+    let arrayTemporadas:number[] = []
+    for(let sr of array){
+        arrayTemporadas.push(sr.seasons);
+    }
+    let suma =  arrayTemporadas.reduce((previous, current) => current += previous);
+    let promedio:number = suma / arrayTemporadas.length;
+    let trElement:HTMLElement = document.createElement("tr");
+    trElement.innerHTML = `<td><b>Seasons avarege: </b></td><td>${promedio}</td>`;
+    promedioTable.appendChild(trElement);
+}
+
+function InfoSerie(serie: Serie):void
+{
+    let html =   `<img class="card-img-top" src="${serie.image}" alt="Card image cap">
+                    <div class="card-body">
+                    <h5>${serie.name}</h5>
+                    <p class="card-text">${serie.description}\n</p>
+                    <p class="card-text" style="color:blue">${serie.link}</p>
+                    </div>`
+    serieCard.innerHTML = html;
+}
+
+let botones: HTMLCollection = document.getElementsByClassName("btn")!;
+
+for (var i = 0; i < botones.length; i++) 
+{
+    let button: HTMLElement = document.getElementById(botones[i].id)!;
+    let serie: Serie = dataSeries[Number(botones[i].id)-1];
+    button.addEventListener("click", (e:Event) => InfoSerie(serie));
+}
